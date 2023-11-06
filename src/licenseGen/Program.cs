366ee818a75d0213dd98094a585e51c9ab2b5942
Niels Bouma <9073152+nielscil@users.noreply.github.com>
2023-11-06 10:43:10 +0100

    updated license to v13

--- src/licenseGen/Program.cs
+++ src/licenseGen/Program.cs
@@ -421,7 +421,11 @@ namespace bitwardenSelfLicensor
             set("SelfHost", true);
             set("UsersGetPremium", true);
             set("UseCustomPermissions", true);
-            set("Version", 11);
+            set("UsePasswordManager", true);
+            set("UseSecretsManager", true);
+            set("SmSeats", (int)32767);
+            set("SmServiceAccounts", (int)32767);
+            set("Version", 13);
             set("Issued", DateTime.UtcNow);
             set("Refresh", DateTime.UtcNow.AddYears(100).AddMonths(-1));
             set("Expires", DateTime.UtcNow.AddYears(100));
